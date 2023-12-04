9059b155d037f35ba5cce825876e38a6cd9b4187
SSSayon <1697295548@qq.com>
2023-12-04 20:24:34 +0800

    hw1 modified

--- hw1/GuitarHero.java
+++ hw1/GuitarHero.java
@@ -1,6 +1,6 @@
 public class GuitarHero {
     private static final double CONCERT_A = 440.0;
-    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    private static final String KEYBOARD = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
 
     private static double calcFreq(int i) {
         return CONCERT_A * Math.pow(2, (i - 24) / 12);
@@ -8,7 +8,7 @@ public class GuitarHero {
 
     public static void main(String[] args) {
 
-        int n = keyboard.length();
+        int n = KEYBOARD.length();
         synthesizer.GuitarString[] strings = new synthesizer.GuitarString[n];
         for (int i = 0; i < n; ++i) {
             strings[i] = new synthesizer.GuitarString(calcFreq(i));
@@ -19,7 +19,7 @@ public class GuitarHero {
             /* check if the user has typed a key; if so, process it */
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                int index = keyboard.indexOf(key);
+                int index = KEYBOARD.indexOf(key);
                 if (index < 0) {
                     continue;
                 }
